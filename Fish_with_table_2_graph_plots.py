ae62e45a843af4de74d5888a640e4ff184ee1cec
YashasviKotra18 <143369356+YashasviKotra18@users.noreply.github.com>
2023-09-22 14:22:39 -0700

    Add files via upload

---
+++ Fish_with_table_2_graph_plots.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv('Fish.csv')
+
+st.markdown("### Fish Data Graphs")
+
+Fig1 = px.bar(df, x= 'Species', y = 'Weight', title = 'Bar plot depicting weights of different fish species')
+Fig2 = px.scatter(df, color="Width", x= 'Species', y = 'Width', title = 'Bar plot depicting Widths of different fish species')
+
+st.plotly_chart(Fig1)
+st.plotly_chart(Fig2)
+
+st.markdown("### Detailed Data View")
+st.dataframe(df)
